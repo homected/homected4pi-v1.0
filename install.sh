7e7197e6b303d40bcd4489fd721c56f9f992d2d0
Homected <support@homected.com>
2012-11-29 20:30:24 +0100

    Clarify install script

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@ clear
 echo "Welcome to homected4pi installer!!!"
 echo "==================================="
 echo ""
-echo " Creating directories..."
+echo "*** Creating directories..."
 mkdir config
 mkdir logs
 sudo mkdir /var/www
 sudo chown -R $USER /var/www
 echo ""
-echo " Installing libxml2..."
+echo "*** Installing libxml2..."
 sudo apt-get install libxml2 libxml2-dev -q
 echo ""
-echo " Installing sqlite3..."
+echo "*** Installing sqlite3..."
 sudo apt-get install sqlite3 sqlite3-dev -q
 echo ""
-echo " Installing FTP server..."
+echo "*** Installing FTP server..."
 sudo apt-get install vsftpd -q
 echo ""
-echo " Compiling libraries..."
+echo "*** Compiling libraries..."
 (cd libs; make)
-echo " Compiling binaries..."
+echo "*** Compiling binaries..."
 (cd apps; make)
